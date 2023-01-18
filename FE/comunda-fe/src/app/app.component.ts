b7cf27368638a55c896b39ded1c9a99498db9213
Domino <dominik.9.gajdos@gmail.com>
2023-01-18 11:27:46 +0100

    doladenie test app

--- FE/comunda-fe/src/app/app.component.ts
+++ FE/comunda-fe/src/app/app.component.ts
@@ -13,10 +13,15 @@ export class AppComponent implements OnInit{
   public taskId: any = "";
   public result: any = "";
 
+  public processSelected: boolean = false;
+
+  public processList: any;
+  public taskList: any;
+
   constructor(
     private httpClient: HttpClient
     ) {
-
+    this.loadProcess();
   }
 
   ngOnInit(): void {
@@ -24,15 +29,36 @@ export class AppComponent implements OnInit{
   }
 
   submit(data: any){
-    console.log(data);
     this.result = JSON.stringify(data);
+  }
+
+  selectProcess(){
+    this.processSelected = true;
+    this.loadTasks(this.processId);
   }
 
   private loadJsonForFormular(procesId: string, taskId: string){
     this.httpClient.get(`http://localhost:8080/form/${procesId}/${taskId}`).subscribe({
       next: data => {
-        console.log(data);
         this.userTaskJson = data;
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  private loadProcess(){
+    this.httpClient.get(`http://localhost:8080/process-list`).subscribe({
+      next: (data: any) => {
+        this.processList = data.process;
+      },
+      error: error => console.log(error)
+    });
+  }
+
+  private loadTasks(processId: string){
+    this.httpClient.get(`http://localhost:8080/task-list/${processId}`).subscribe({
+      next: (data:any) => {
+        this.taskList = data.tasks;
       },
       error: error => console.log(error)
     });
